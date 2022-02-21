d87ebd290f640ba3c238e23a5752f75b19fa48bc
nahla_epic <nahla.s1995@gmail.com>
2022-02-21 16:26:23 +0200

    adding news remote data source

--- app/src/main/java/com/nahlasamir244/newsapp/data/datasource/remote/NewsRemoteDataSource.kt
+++ app/src/main/java/com/nahlasamir244/newsapp/data/datasource/remote/NewsRemoteDataSource.kt
@@ -1,4 +1,8 @@
 package com.nahlasamir244.newsapp.data.datasource.remote
 
+import com.nahlasamir244.newsapp.data.model.NewsWireResponse
+import retrofit2.Response
+
 interface NewsRemoteDataSource {
+    suspend fun getNewsList(): Response<NewsWireResponse>
 }
--- app/src/main/java/com/nahlasamir244/newsapp/data/datasource/remote/NewsRemoteDataSourceImpl.kt
+++ app/src/main/java/com/nahlasamir244/newsapp/data/datasource/remote/NewsRemoteDataSourceImpl.kt
@@ -1,4 +1,13 @@
 package com.nahlasamir244.newsapp.data.datasource.remote
 
-class NewsRemoteDataSourceImpl : NewsRemoteDataSource {
+import com.nahlasamir244.newsapp.data.api.NewsApiService
+import com.nahlasamir244.newsapp.data.model.NewsWireResponse
+import retrofit2.Response
+import javax.inject.Inject
+
+class NewsRemoteDataSourceImpl @Inject constructor(
+    private val newsApiService: NewsApiService
+) : NewsRemoteDataSource {
+    override suspend fun getNewsList(): Response<NewsWireResponse> =
+        newsApiService.getNewsList()
 }
