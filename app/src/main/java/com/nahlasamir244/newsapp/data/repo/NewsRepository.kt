f58abda99bd52f4b9ae2fe043ae1d09a2974e4a8
nahla_epic <nahla.s1995@gmail.com>
2022-02-21 16:33:04 +0200

    adding news repository

--- app/src/main/java/com/nahlasamir244/newsapp/data/repo/NewsRepository.kt
+++ app/src/main/java/com/nahlasamir244/newsapp/data/repo/NewsRepository.kt
@@ -1,4 +1,8 @@
 package com.nahlasamir244.newsapp.data.repo
 
+import com.nahlasamir244.newsapp.data.model.NewsWireResponse
+import retrofit2.Response
+
 interface NewsRepository {
+    suspend fun getNewsList(): Response<NewsWireResponse>
 }
--- app/src/main/java/com/nahlasamir244/newsapp/data/repo/NewsRepositoryImpl.kt
+++ app/src/main/java/com/nahlasamir244/newsapp/data/repo/NewsRepositoryImpl.kt
@@ -1,4 +1,14 @@
 package com.nahlasamir244.newsapp.data.repo
 
-class NewsRepositoryImpl:NewsRepository {
+import com.nahlasamir244.newsapp.data.datasource.remote.NewsRemoteDataSource
+import com.nahlasamir244.newsapp.data.model.NewsWireResponse
+import retrofit2.Response
+import javax.inject.Inject
+
+class NewsRepositoryImpl @Inject constructor(
+    private val newsRemoteDataSource: NewsRemoteDataSource
+) : NewsRepository {
+    override suspend fun getNewsList(): Response<NewsWireResponse> =
+        newsRemoteDataSource.getNewsList()
+
 }
