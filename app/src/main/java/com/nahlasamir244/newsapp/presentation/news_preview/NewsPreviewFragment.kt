81dafb4168c028e9b770c9929cfc8eedcd93193b
nahla_epic <nahla.s1995@gmail.com>
2022-02-21 21:50:00 +0200

    implementing single news preview

--- app/src/main/java/com/nahlasamir244/newsapp/presentation/news_preview/NewsPreviewFragment.kt
+++ app/src/main/java/com/nahlasamir244/newsapp/presentation/news_preview/NewsPreviewFragment.kt
@@ -7,13 +7,14 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.nahlasamir244.newsapp.R
+import com.nahlasamir244.newsapp.databinding.NewsPreviewFragmentBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class NewsPreviewFragment : Fragment() {
 
-    private val viewModel: NewsPreviewViewModel by viewModels()
-
+    private val newsPreviewViewModel: NewsPreviewViewModel by viewModels()
+    private lateinit var binding: NewsPreviewFragmentBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -23,6 +24,8 @@ class NewsPreviewFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = NewsPreviewFragmentBinding.bind(view)
+        binding.viewModel = newsPreviewViewModel
     }
 
 }
--- app/src/main/java/com/nahlasamir244/newsapp/presentation/news_preview/NewsPreviewViewModel.kt
+++ app/src/main/java/com/nahlasamir244/newsapp/presentation/news_preview/NewsPreviewViewModel.kt
@@ -1,7 +1,13 @@
 package com.nahlasamir244.newsapp.presentation.news_preview
 
+import androidx.hilt.Assisted
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import com.nahlasamir244.newsapp.data.model.News
 
-class NewsPreviewViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class NewsPreviewViewModel @ViewModelInject constructor(
+    @Assisted private val savedStateHandle: SavedStateHandle
+) : ViewModel() {
+    val news: News? = savedStateHandle.get<News>("news")
 }
