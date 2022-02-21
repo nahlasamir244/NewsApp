d2b0e79b09ffd999e9145bc52ec6d40330c35625
nahla_epic <nahla.s1995@gmail.com>
2022-02-21 17:02:14 +0200

    add domain layer usecase load image

--- app/src/main/java/com/nahlasamir244/newsapp/application/NewsApp.kt
+++ app/src/main/java/com/nahlasamir244/newsapp/application/NewsApp.kt
@@ -1,8 +1,21 @@
 package com.nahlasamir244.newsapp.application
 
 import android.app.Application
+import android.content.Context
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class NewsApp :Application() {
+
+    companion object {
+        private lateinit var context: Context
+        fun getAppContext(): Context {
+            return context
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+    }
 }
--- app/src/main/java/com/nahlasamir244/newsapp/domain/LoadImageUseCase.kt
+++ app/src/main/java/com/nahlasamir244/newsapp/domain/LoadImageUseCase.kt
@@ -1,4 +1,19 @@
 package com.nahlasamir244.newsapp.domain
 
+import android.widget.ImageView
+import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
+import com.nahlasamir244.newsapp.application.NewsApp
+
+typealias LoadImage = LoadImageUseCase
+
 class LoadImageUseCase {
+    operator fun invoke(imageView: ImageView, url: String, placeholderResourceId: Int) =
+        Glide.with(imageView).load(url)
+            .placeholder(
+                ContextCompat.getDrawable(
+                    NewsApp.getAppContext(),
+                    placeholderResourceId
+                )
+            ).into(imageView)
 }
